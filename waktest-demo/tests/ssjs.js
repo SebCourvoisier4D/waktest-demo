3ee55cc01ac24b97f90db48771fdd3b17a5c246e
SebCourvoisier4D <sebastien.courvoisier@4d.com>
2015-03-24 23:15:41 +0100

    Update #18

--- waktest-demo/tests/ssjs.js
+++ waktest-demo/tests/ssjs.js
@@ -12,12 +12,12 @@ describe("My application", function () {
 	
 	it("has a bootstrap file named 'bootstrap.js'", function () {
 		var myBootstrap = application.getItemsWithRole('bootStrap');
-		myBootstrap.should.be.an("object");
-		myBootstrap.should.have.a.property("name");
+		myBootstrap.should.be.an("object")
+		.and.have.a.property("name");
 		myBootstrap.name.should.be.equal("bootstrap.js");
 	});
 	
-	it("has a local 'tests' FileSystem...", function (done) {
+	it("has a local 'tests' FileSystem URL...", function (done) {
 		resolveLocalFileSystemURL('tests',
 			function(myDir) {
 				eventually(done, function () {
@@ -46,6 +46,59 @@ describe("My application", function () {
 				done(e);
 			}
 		);
+	});
+	
+	it("has a DataStore", function () {
+		ds.should.be.an("object");
+	});
+	
+	it("has an empty 'Company' DataClass", function () {
+		ds.Company.should.be.an("object").and.respondTo('query');
+		expect(ds.Company.length).to.equal(0);
+	});
+	
+	it("has an empty 'Employee' DataClass", function () {
+		ds.Employee.should.be.an("object").and.respondTo('query');
+		expect(ds.Employee.length).to.equal(0);
+	});
+	
+	it("creates a Company...", function () {
+		ds.Company.should.respondTo('createEntity');
+		var myCompany = ds.Company.createEntity();
+		myCompany.should.be.an("object").and.respondTo('save');
+		myCompany.name = 'Wakanda';
+		myCompany.save();
+	});
+	
+	it("...and finds it", function () {
+		ds.Company.should.respondTo('find');
+		var myCompany = ds.Company.find('name=:1', 'Wakanda');
+		myCompany.should.be.an("object").and.have.a.property('name');
+		expect(myCompany.name).to.equal('Wakanda');
+	});
+	
+	it("creates an Employee...", function () {
+		ds.Employee.should.respondTo('createEntity');
+		var myEmployee = ds.Employee.createEntity();
+		myEmployee.should.be.an("object").and.respondTo('save');
+		myEmployee.name = 'Bob';
+		myEmployee.save();
+	});
+	
+	it("...and finds it", function () {
+		ds.Employee.should.respondTo('find');
+		var myEmployee = ds.Employee.find('name=:1', 'Bob');
+		myEmployee.should.be.an("object").and.have.a.property('name');
+		expect(myEmployee.name).to.equal('Bob');
+	});
+	
+	it("binds an Employee to a Company", function () {
+		var myEmployee = ds.Employee.find('name=:1', 'Bob');
+		var myCompany = ds.Company.find('name=:1', 'Wakanda');
+		myEmployee.company = myCompany;
+		myEmployee.save();		
+		expect(myEmployee.company).to.have.a.property('name');
+		expect(myEmployee.company.name).to.equal('Wakanda');
 	});
 	
 });
